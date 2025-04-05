0697cb8774327551140105587dc8f5be5be7e998
Ricardo Gonzalez <rickgc@gmail.com>
2025-04-05 10:07:05 +0100

    🐛 Fix unesed dependency

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,6 +1,6 @@
 // 'use client'
 
-import { use, useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export default function Logo() {
   const outlineRef = useRef<SVGPathElement>(null)
