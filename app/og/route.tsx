1d23c9bcd48863881cc3066567405f9575edf6f1
Ricardo Gonzalez <rickgc@gmail.com>
2025-06-30 10:03:30 +0100

    ✨ Improve OG image

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,3 +1,6 @@
+import Logo from '@/components/Logo'
+import { color } from 'motion/react'
+
 import { ImageResponse } from 'next/og'
 
 export function GET(request: Request) {
@@ -37,7 +40,7 @@ export function GET(request: Request) {
               'linear-gradient(133deg, rgb(30, 31, 31) 0%, rgb(26, 26, 27) 45%, rgb(25, 25, 26) 100%)',
           }}
         >
-          <div tw="rounded p-10 bg-zinc-900 h-full w-full flex flex-col pattern-diagonal-lines pattern-yellow-500 pattern-bg-zinc-900 pattern-size-32 pattern-opacity-40">
+          <div tw="rounded p-10 bg-zinc-900 h-full w-full flex flex-col pattern-diagonal-lines pattern-yellow-500 pattern-bg-zinc-900 pattern-size-32 pattern-opacity-40 relative z-10">
             <div tw="mt-16 flex text-6xl leading-normal text-gray-200">
               {title}
             </div>
@@ -63,6 +66,46 @@ export function GET(request: Request) {
               </div>
             )}
           </div>
+
+          <svg
+            viewBox="0 0 480 480"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            id="svg-logo"
+            style={{
+              position: 'absolute',
+              bottom: 50,
+              right: 50,
+              width: 150,
+              height: 150,
+              color: 'white',
+            }}
+          >
+            <path
+              d="M14 10H466V462H14V10Z"
+              stroke="currentColor"
+              strokeWidth="20"
+              strokeLinecap="square"
+              id="outline"
+              style={{
+                opacity: 0.5,
+              }}
+            />
+            <path
+              d="M105 386V86H255"
+              stroke="currentColor"
+              strokeWidth="60"
+              strokeLinecap="square"
+              id="top"
+            />
+            <path
+              d="M375 86V386H225"
+              stroke="currentColor"
+              strokeWidth="60"
+              strokeLinecap="square"
+              id="bottom"
+            />
+          </svg>
         </div>
       ),
       {
