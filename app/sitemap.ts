920f3e5619854eb3048360cdbe7f62d28f2af633
Ricardo Gonzalez <rickgc@gmail.com>
2025-07-01 23:53:53 +0100

    🐛 Fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,25 +1,28 @@
-import { getBlogPosts } from "@/lib/blog";
+import { getBlogPosts } from '@/lib/blog'
 
-export const baseUrl = "https://ryck.xyz";
-
+export const baseUrl = process.env.WEBSITE_URL || 'https://ryck.dev'
 export default async function sitemap() {
-  const posts = await getBlogPosts();
+  const posts = await getBlogPosts()
 
   const blogs = posts.map((post) => ({
     url: `${baseUrl}/blog/${post.slug}`,
     lastModified: post.publishedAt,
-  }));
+  }))
 
-  const routes = ["", "/blog", "/blog/categories", "/projects", "/resume"].map((route) => ({
-    url: `${baseUrl}${route}`,
-    lastModified: new Date().toISOString().split("T")[0],
-  }));
+  const routes = ['', '/blog', '/blog/categories', '/projects', '/resume'].map(
+    (route) => ({
+      url: `${baseUrl}${route}`,
+      lastModified: new Date().toISOString().split('T')[0],
+    }),
+  )
 
-  const uniqueCategories = [...new Set(posts.flatMap(post => post.categories || []))];
+  const uniqueCategories = [
+    ...new Set(posts.flatMap((post) => post.categories || [])),
+  ]
   const categories = uniqueCategories.map((category) => ({
     url: `${baseUrl}/categories/${category}`,
-    lastModified: new Date().toISOString().split("T")[0],
-  }));
+    lastModified: new Date().toISOString().split('T')[0],
+  }))
 
-  return [...routes, ...blogs, ...categories];
+  return [...routes, ...blogs, ...categories]
 }
